2798b61b743d5278b8aa06a86324519665429ae5
Stanisław Chmiela <sjchmiela@gmail.com>
2016-06-02 12:34:18 +0200

    Add Obj-C bridging header

---
+++ ios/CallOfBeacons/CallOfBeacons/ObjCBridge.h
@@ -0,0 +1,14 @@
+//
+//  ObjCBridge.h
+//  CallOfBeacons
+//
+//  Created by Stanisław Chmiela on 02.06.2016.
+//  Copyright © 2016 Stanisław Chmiela, Rafał Żelazko. All rights reserved.
+//
+
+#ifndef ObjCBridge_h
+#define ObjCBridge_h
+
+#import <EstimoteSDK/EstimoteSDK.h>
+
+#endif /* ObjCBridge_h */
