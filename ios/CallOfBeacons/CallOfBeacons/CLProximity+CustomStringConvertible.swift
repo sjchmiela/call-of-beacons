610b88f6164e9bf043978b54dec61c912ecdf8e1
Stanisław Chmiela <sjchmiela@gmail.com>
2016-06-02 15:44:30 +0200

    Add CustomStringConversion of CLProximity

---
+++ ios/CallOfBeacons/CallOfBeacons/CLProximity+CustomStringConvertible.swift
@@ -0,0 +1,24 @@
+//
+//  CLProximity+CustomStringConvertible.swift
+//  CallOfBeacons
+//
+//  Created by Stanisław Chmiela on 02.06.2016.
+//  Copyright © 2016 Stanisław Chmiela, Rafał Żelazko. All rights reserved.
+//
+
+import Foundation
+
+extension CLProximity {
+    var description: String {
+        switch self {
+        case .Immediate:
+            return "Immediate"
+        case .Near:
+            return "Near"
+        case .Far:
+            return "Far"
+        default:
+            return "Unknown"
+        }
+    }
+}
