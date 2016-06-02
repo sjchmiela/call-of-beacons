812c7971510e6ace2cd88cbffdcb81baf039122a
Stanisław Chmiela <sjchmiela@gmail.com>
2016-06-03 00:03:05 +0200

    Add logging of received JSON from websocket

--- web/scripts/app.js
+++ web/scripts/app.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this._ws = new WebSocket('ws://127.0.0.1:1880/ws/positions');
+    this._ws.onmessage = (event) => this._handleData(JSON.parse(event.data));
+  }
+
+  _handleData(data) {
+    console.log(data);
+  }
+
   render() {
     return (
-      <h1>Hello, world.</h1>
+      <div class="App">
+        <h1>Hello, world.</h1>
+      </div>
     );
   }
 }
