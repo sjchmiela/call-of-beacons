27ac9e768adac3bf8b3641913616520e8b3ecd27
Stanisław Chmiela <sjchmiela@gmail.com>
2016-06-02 15:21:17 +0200

    Clean up and add convenience initializers

--- ios/CallOfBeacons/CallOfBeacons/COBBeacon.swift
+++ ios/CallOfBeacons/CallOfBeacons/COBBeacon.swift
@@ -9,20 +9,33 @@
 import Foundation
 
 class COBBeacon: Equatable, CustomStringConvertible {
-    init(major: Int?, minor: Int?, name: String) {
+    var major: Int?
+    var minor: Int?
+    var name: String?
+    var behavior: String?
+    
+    init(major: Int?, minor: Int?, behavior: String?, name: String?) {
         self.major = major
         self.minor = minor
+        self.behavior = behavior
         self.name = name
     }
     
-    convenience init(major: Int?, minor: Int?) {
-        self.init(major: major, minor: minor, name: "unknown")
-    }
-    var major: Int?
-    var minor: Int?
-    var name: String
     var description: String {
-        return "Beacon \(name) (\((major ?? 0)!):\((minor ?? 0)!))"
+        return "Beacon \(name) acting as \(behavior) (\((major ?? 0)!):\((minor ?? 0)!))"
+    }
+    
+    convenience init(jsonData: JSON) {
+        self.init(
+            major: jsonData["major"].int,
+            minor: jsonData["minor"].int,
+            behavior: jsonData["behavior"].stringValue,
+            name: jsonData["name"].stringValue
+        )
+    }
+
+    convenience init(beacon: CLBeacon) {
+        self.init(major: beacon.major.integerValue, minor: beacon.minor.integerValue, behavior: nil, name: nil)
     }
 }
 
