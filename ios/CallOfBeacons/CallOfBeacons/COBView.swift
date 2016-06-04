26d2fe5422acdff77cb47d737bda6bd1142f5493
Stanisław Chmiela <sjchmiela@gmail.com>
2016-06-04 14:57:10 +0200

    Add View subclass that may have border

---
+++ ios/CallOfBeacons/CallOfBeacons/COBView.swift
@@ -0,0 +1,31 @@
+//
+//  COBView.swift
+//  CallOfBeacons
+//
+//  Created by Stanisław Chmiela on 04.06.2016.
+//  Copyright © 2016 Stanisław Chmiela, Rafał Żelazko. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class COBView: UIView {
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+            layer.masksToBounds = cornerRadius > 0
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        didSet {
+            layer.borderColor = borderColor?.CGColor
+        }
+    }
+}
