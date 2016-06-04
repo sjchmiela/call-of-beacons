b41f9864114e2cf3159714594e2d9180697c8bd9
Stanisław Chmiela <sjchmiela@gmail.com>
2016-06-04 10:54:16 +0200

    Add status to Circle

--- web/components/Block/Circle.js
+++ web/components/Block/Circle.js
@@ -3,9 +3,24 @@ import React, { PropTypes } from 'react';
 export default function Circle(props) {
   const minusOffset = props.size * (-1 / 2);
 
+  let color = null;
+  switch (props.status) {
+    case 'active':
+      color = '#3CAE1D';
+      break;
+    case 'loading':
+      color = '#D2E13C';
+      break;
+    case 'error':
+      color = '#B1132C';
+      break;
+  }
+
   let styles = {
     position: 'absolute',
-    border: '1px solid #3F525B',
+    border: `1px solid ${color || '#3F525B'}`,
+    background: color || 'none',
+    ...props.style,
     width: props.size,
     height: props.size,
   };
@@ -30,6 +45,7 @@ Circle.propTypes = {
   bottom: PropTypes.bool,
   left: PropTypes.bool,
   size: PropTypes.number,
+  status: PropTypes.oneOf(['active', 'loading', 'error']),
 };
 
 Circle.defaultProps = {
