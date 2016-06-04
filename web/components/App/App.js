553b6cb2b1f73050fa28f9b8793640a018347c82
Stanisław Chmiela <sjchmiela@gmail.com>
2016-06-04 10:54:38 +0200

    Show connection status in header

--- web/components/App/App.js
+++ web/components/App/App.js
@@ -6,18 +6,50 @@ export default class App extends Component {
   constructor(props) {
     super(props);
 
-    this._ws = new WebSocket('ws://127.0.0.1:1880/ws/positions');
-    this._ws.onmessage = (event) => this._handleData(JSON.parse(event.data));
+    const websocket = new WebSocket('ws://127.0.0.1:1880/ws/positions');
+    websocket.onerror = () => this._onError();
+    websocket.onopen = () => this._onOpen();
+    websocket.onclose = () => this._onClose();
+
+    this.state = {
+      websocket,
+      error: undefined,
+    };
+  }
+
+  _onOpen() {
+    console.error('Websocket connection is open.');
+    this.setState({ error: false });
+  }
+
+  _onClose() {
+    console.error('Websocket connection is closed.');
+    this.setState({ error: true });
+  }
+
+  _onError() {
+    console.error('Websocket connection encountered an error.');
+    this.setState({ error: true });
   }
 
   _handleData(data) {
     console.log(data);
   }
 
+  _status() {
+    if (this.state.error) {
+      return 'error';
+    } else if (this.state.error === false) {
+      return 'active';
+    } else {
+      return 'loading';
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <Header />
+        <Header status={this._status()} />
         <h1>Hello, world.</h1>
       </div>
     );
